8dc89033130c0dba60d847405a1463ae12111382
Elvis B. Martins <elvisbmartins@gmail.com>
2018-02-27 12:05:43 -0300

    index php test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'assets/views/content' ) ?>
+					<?php get_template_part( 'templates/content' ) ?>
 
 				<?php endwhile; ?>
 
@@ -20,12 +20,52 @@ get_header(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'assets/views/content', 'none' ); ?>
+			<?php get_template_part( 'templates/content', 'none' ); ?>
 
 		<?php endif; ?>
 
 	</div>
 	<!-- /.aa_wrap -->
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm">
+				Teste AB 
+				<div style="font-size:3em; font-family: 'FontAwesome'; color:#4300ff">
+				  <i class="fa fa-camera-retro fa-10x"></i>
+				</div>
+			</div>
+			<div class="col-sm">
+				Teste CD
+				<div style="font-size:3em; font-family: 'FontAwesome'; color:#4300ff">
+				<span class="fa-stack fa-2x">
+				  <i class="fas fa-square fa-stack-2x"></i>
+				  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+				</span>
+				<span class="fa-stack fa-2x">
+				  <i class="fas fa-circle fa-stack-2x"></i>
+				  <i class="fas fa-flag fa-stack-1x fa-inverse"></i>
+				</span>
+				<span class="fa-stack fa-2x">
+				  <i class="fas fa-square fa-stack-2x"></i>
+				  <i class="fas fa-terminal fa-stack-1x fa-inverse"></i>
+				</span>
+				<span class="fa-stack fa-2x">
+				  <i class="fas fa-camera fa-stack-1x"></i>
+				  <i class="fas fa-ban fa-stack-2x" style="color:Tomato"></i>
+				</span>
+			</div>
+			</div>
+			<div class="col-sm">
+				Teste EF
+				<div style="font-size:3em; font-family: 'FontAwesome'; color:#4300ff">
+				<div style="font-size:3em;">
+					<i class="fas fa-user" />
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
